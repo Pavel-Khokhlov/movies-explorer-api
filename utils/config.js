49eb2253a8af88fb6cb89d852315be3901d86fcc
Pavel Khokhlov <ya.pavel-kh@yandex.ru>
2021-07-04 02:29:42 +0300

    fix: utils/config.js

--- utils/config.js
+++ utils/config.js
@@ -14,9 +14,7 @@ module.exports.Options = {
   origin: [
     'http://localhost:3000',
     'http://localhost:3001',
-    'http://localhost:3002',
-    'http://localhost:8080',
-    'https://pavel-khokhlov.nomoredomains.monster',
+    'http://localhost:3003',
   ],
   methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
   preflightContinue: false,
